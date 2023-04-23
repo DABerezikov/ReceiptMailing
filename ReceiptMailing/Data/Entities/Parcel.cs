019384fbfa8efce521f37de7a9590f8896dd2aee
Дмитрий Березиков <dm.berezikov@ya.ru>
2023-04-23 16:49:09 +0700

    рефакторинг

--- ReceiptMailing/Data/Entities/Parcel.cs
+++ ReceiptMailing/Data/Entities/Parcel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ReceiptMailing.Data.Entities.Base;
+﻿using ReceiptMailing.Data.Entities.Base;
 
 namespace ReceiptMailing.Data.Entities
 {
--- ReceiptMailing/Data/Entities/Passport.cs
+++ ReceiptMailing/Data/Entities/Passport.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReceiptMailing.Data.Entities
+﻿namespace ReceiptMailing.Data.Entities
 {
     public class Passport
     {
