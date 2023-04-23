1c31c256a0634d1a61c8a2203993420ed5826dfb
Дмитрий Березиков <dm.berezikov@ya.ru>
2023-04-24 00:25:03 +0700

    Рефакторинг

---
+++ ReceiptMailing/Infrastructure/Commands/ImportDbCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReceiptMailing.Infrastructure.Commands.Base;
+
+namespace ReceiptMailing.Infrastructure.Commands
+{
+    internal class ImportDbCommand : Command
+    {
+        protected override void Execute(object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
